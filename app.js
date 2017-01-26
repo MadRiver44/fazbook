9372fa22293c2677b7b1e44275da6e5388b4f545
Kevin Turney <super996@optonline.net>
2017-01-26 17:04:51 -0500

    post and delete working

--- app.js
+++ app.js
@@ -4,15 +4,18 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var methodOverride = require('method-override');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+app.use(methodOverride('_method'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -24,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/users/new', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
